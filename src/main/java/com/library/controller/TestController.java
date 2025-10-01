3a68fcb0ca9e59141bbabb1ff427f3854f92a768
Gangadhar5168 <prathapvg5168@gmail.com>
2025-10-01 10:52:36 +0530

    Add security config with modern lambda DSL syntax

---
+++ src/main/java/com/library/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.library.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Library Management System is running!";
+    }
+    
+}
